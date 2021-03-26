749cefb6132fb712e1f9475aee9b8978428efe3e
TrainingCode <greenwind.01011994@gmail.com>
2021-03-26 14:49:21 +0700

    chưa hiểu

--- a.js
+++ a.js
@@ -3,15 +3,9 @@ const b = 100;
 
 if(a>b)
 <<<<<<< HEAD
-    console.log("a =1 10");
+    console.log("chả hiểu");
 else
     console.log("b =1 100");
-=======
-    console.log("a =10");
-else
-    console.log("b = 1 = =100");
-
->>>>>>> c91b5dd8ab269c1bde9121f4d97491ba86fba5f6
 
 const max = a > b ? a : b;
 console.log(max);
