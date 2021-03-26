10deaff7a4adb1772e8a8505185b51cdf02bbe84
TrainingCode <greenwind.01011994@gmail.com>
2021-03-26 12:12:37 +0700

    thử

--- a.js
+++ a.js
@@ -2,9 +2,9 @@ const a = 10;
 const b = 100;
 
 if(a>b)
-    console.log("a === 10");
+    console.log("a = 10");
 else
-    console.log("b === 100");
+    console.log("b = 100");
 
 const max = a > b ? a : b;
 console.log(max);
